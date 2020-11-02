47397c29860bed1771e95a7fca7f899875683f48
410713071 <410713071@gms.ndhu.edu.tw>
2020-11-02 12:26:26 +0800

    Initial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(0, 2, -10);
+    public float distanceUp = 2f;
+    public float distanceAway = 10f;
+    public float distanceback = 6f;
+    public float smooth = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = plane.transform.position + offset;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 dispos = plane.transform.position + Vector3.up * distanceUp + Vector3.right * distanceAway + Vector3.back * distanceback ;
+        transform.position = Vector3.Lerp(transform.position, dispos, Time.deltaTime * smooth);
+        transform.LookAt(plane.transform.position);
+       
+
+
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+    public float HorizontalInput;
+    public float flipInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+        flipInput = Input.GetAxis("flip");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * verticalInput+ Vector3.up * rotationSpeed * Time.deltaTime * HorizontalInput + Vector3.back * rotationSpeed * 0.7f * Time.deltaTime * flipInput);
+    }
+    
+
+
+}
